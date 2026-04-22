40635b1c9beb1731fb00b2b21d5dd087ac213505
mac n cheese <admin@mac-n-cheese.local>
2026-04-23 06:39:26 +0700

    Ignore build errors to finish deployment

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   typescript: {
-    // Bỏ qua lỗi TypeScript để Deploy thành công
+    // Ép Vercel bỏ qua lỗi TypeScript để Deploy
     ignoreBuildErrors: true,
   },
   eslint: {
