15a2f7818c8be64c3f6e967924f88edae615c7c5
mac n cheese <admin@mac-n-cheese.local>
2026-04-24 05:18:09 +0700

    Final fix: Use .mjs config to force deploy

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   typescript: {
-    // Ép Vercel bỏ qua lỗi TypeScript để Deploy
     ignoreBuildErrors: true,
   },
   eslint: {
-    // Bỏ qua lỗi ESLint khi build
     ignoreDuringBuilds: true,
   },
 };
